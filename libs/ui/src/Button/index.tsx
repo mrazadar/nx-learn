6aa354c87e45fc152f3d9c9b3e8a0a12044854b5
Raza Dar <5585262+mrazadar@users.noreply.github.com>
2022-10-07 20:09:30 +0500

    added ui lib

---
+++ libs/ui/src/Button/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+interface ButtonProps extends TouchableOpacityProps{
+    onPress: () => void,
+    text: string
+}
+
+export const Button : React.FunctionComponent<ButtonProps> = ({
+    onPress,
+    text
+}) => {
+
+    return (
+        <TouchableOpacity 
+            style={styles.button}
+            onPress={onPress}
+        >
+          <Text style={[styles.textMd, styles.textCenter]}>{text}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+  textCenter: {
+    textAlign: 'center',
+  },
+  textMd: {
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 16,
+    borderRadius: 8,
+    width: '50%',
+    marginTop: 24,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    paddingHorizontal: 16,
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  }
+});
+
